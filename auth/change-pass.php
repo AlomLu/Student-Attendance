b7fbc71fa1a37f7a24a42ad6cc0d90edbad9545f
AlomLu <abdurrahmanalom@gmail.com>
2024-10-28 05:12:39 +0600

    Fixed role-based navigation redirect links

--- auth/change-pass.php
+++ auth/change-pass.php
@@ -1,12 +1,66 @@
-<!-- <ul>
-    <li><a href="../student/student-dashboard.html">dashboard</a></li>
-    <li><a href="../student/student-profile.html">profile</a></li>
-    <li><a href="../teacher/attendance.html">attendance</a></li>
-    <li><a href="../auth/change-pass.html">change password</a></li>
-    <li><a href="">Alom</a></li>
-    <li><a href="">Logout</a></li>
-</ul> -->
-<?php include '../inc/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <?php 
+            if(isset($_GET['user_role_id'])){
+                $user_role_id = $_GET['user_role_id'];
+            }
+        ?>
+        <!-- Header area start -->
+        <div class="student-header">
+            <div class="logo">
+                <a href="">Student</a>
+            </div>
+            <nav class="menu">
+            <ul>
+                    <li><a href="
+                        <?php
+                            if($user_role_id == 1){
+                                echo "../student/student-dashboard.php";
+                            }elseif($user_role_id == 2){
+                                echo "../teacher/teacher-dashboard.php";
+                            }
+                        ?>
+                    ">dashboard</a></li>
+                    <li><a href="
+                        <?php
+                            if($user_role_id == 1){
+                                echo "../student/student-profile.php";
+                            }elseif($user_role_id == 2){
+                                echo "../teacher/teacher-profile.php";
+                            }
+                        ?>
+                    ">profile</a></li>
+                    <li><a href="change-pass.php">change password</a></li>
+
+                    <?php 
+                        if($user_role_id == 2){
+                            echo '<li><a href="../teacher/attendance.php">attendance</a></li>';
+                            echo '<li><a href="../teacher/add-class.php">Add class</a></li>';
+                        }
+                    ?>
+
+                    <li><a href="">Alom</a></li>
+                    <li>
+                        <?php 
+                            if(isset($_GET['action']) && $_GET['action'] == 'logout'){
+                                Session::destroy();
+                            }
+                        ?>
+                        <a href="?action=logout">Logout</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <!-- Header area end -->
+
         <!-- Change password start -->
         <div class="change-password">
             <h3>change your password</h3>
@@ -24,4 +78,11 @@
         </div>
          <!-- Change password end -->
 
-<?php include '../inc/footer.php'; ?>
+        <!-- Footer area start -->
+        <div class="footer-area">
+            <p></p>&copy; Abdur Rahman Alom, All Rights Reserved.</p>
+        </div>
+        <!-- Footer area end -->
+    </div>
+</body>
+</html>
--- inc/header.php
+++ inc/header.php
@@ -13,6 +13,9 @@
 </head>
 <body>
     <div class="main">
+        <?php 
+            $user_role_id = Session::get('user_role_id')
+        ?>
         <!-- Header area start -->
         <div class="student-header">
             <div class="logo">
@@ -20,9 +23,33 @@
             </div>
             <nav class="menu">
                 <ul>
-                    <li><a href="student-dashboard.php">dashboard</a></li>
-                    <li><a href="student-profile.php">profile</a></li>
-                    <li><a href="../auth/change-pass.php">change password</a></li>
+                    <li><a href="
+                        <?php
+                            if(Session::get('user_role_id') == 1){
+                                echo "student-dashboard.php";
+                            }elseif(Session::get('user_role_id') == 2){
+                                echo "teacher-dashboard.php";
+                            }
+                        ?>
+                    ">dashboard</a></li>
+                    <li><a href="
+                        <?php
+                            if(Session::get('user_role_id') == 1){
+                                echo "student-profile.php";
+                            }elseif(Session::get('user_role_id') == 2){
+                                echo "teacher-profile.php";
+                            }
+                        ?>
+                    ">profile</a></li>
+                    <li><a href="../auth/change-pass.php?user_role_id=<?php echo $user_role_id?>">change password</a></li>
+
+                    <?php 
+                        if(Session::get('user_role_id') == 2){
+                            echo '<li><a href="attendance.php">attendance</a></li>';
+                            echo '<li><a href="add-class.php">Add class</a></li>';
+                        }
+                    ?>
+
                     <li><a href="">Alom</a></li>
                     <li>
                         <?php 
@@ -40,7 +67,14 @@
                     <li><a href="add-class.php">Add class</a></li>
                     <li><a href="../auth/change-pass.php">change password</a></li>
                     <li><a href="">Alom</a></li>
-                    <li><a href="">Logout</a></li>
+                    <li>
+                        <?php 
+                            if(isset($_GET['action']) && $_GET['action'] == 'logout'){
+                                Session::destroy();
+                            }
+                        ?>
+                        <a href="?action=logout">Logout</a>
+                    </li>
                 </ul> -->
             </nav>
         </div>
