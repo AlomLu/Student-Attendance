0f2a8dbc3f1aa19b1e52a147cf7b61568b1cd756
AlomLu <abdurrahmanalom@gmail.com>
2024-11-01 01:31:55 +0600

    Display subjects dynamically in teacher section based on selected class

--- teacher/add-class.php
+++ teacher/add-class.php
@@ -3,18 +3,60 @@
         <!-- class area start -->
         <div class="class-area">
             <h3>add new class</h3>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script>
+                $(document).ready(function(){
+                    $('#class').change(function(){
+                        // var class_id = $(this).val();
+
+                        var class_id = $('#class').val();
+                        console.log(class_id);
+
+                        $.ajax({
+                            url: "fetch-data/fetch-subject.php",
+                            method: "POST",
+                            data: {class_id: class_id},
+                            success: function(data){
+                                $('#selected-class').html(data);
+
+                                console.log(data);
+                            }
+                        })
+                    })
+                })
+            </script>
             <div class="add-class-form">
                 <form action="" method="POST">
                     <div class="form-group">
                         <label for="">class</label>
-                        <select name="class" id="">
+                        <select name="class" id="class">
                             <option value="">Selec a class</option>
-                            <option value="">10</option>
-                            <option value="">9</option>
-                            <option value="">8</option>
+                            <?php 
+                                 $query = "SELECT * FROM tbl_class";
+                                 $class_list = $db->select($query);
+
+                                 if($class_list){
+                                    while($result = $class_list->fetch_assoc()){
+
+                            ?>
+                            <option value="<?php echo $result['id'] ?>"><?php echo $result['class'] ?></option>
+                            <?php } } ?>
                         </select>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group" id="selected-class">
+                        <label for="">subject</label>
+                        <!-- <div class="subject">
+                            <div class="single-subject">
+
+                            </div>
+                        </div> -->
+                        <!-- <select name="" id="selected-class">
+                            <option value="">select subject</option>
+                        </select> -->
+                        <!-- <input type="checkbox" name="" id="selected-class"> -->
+                    </div>
+                    <!-- <div class="form-group">
                         <label for="">section</label>
                         <select name="section" id="">
                             <option value="">Selec a section</option>
@@ -22,7 +64,7 @@
                             <option value="">B</option>
                             <option value="">C</option>
                         </select>
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <input type="submit" name="submit" value="Submit">
                     </div>
---
+++ teacher/fetch-data/fetch-subject.php
@@ -0,0 +1,46 @@
+<?php 
+    // include '../Config/config.php';
+    // include '../lib/Database.php';
+
+    include '../../Config/config.php';
+    include '../../lib/Database.php';
+
+    $db = new Database();
+
+    if(isset($_POST['class_id'])){
+        $class_id = $_POST['class_id'];
+
+        $query = "SELECT * FROM tbl_class WHERE id = '$class_id' ";
+        $subject_string = $db->select($query);
+
+        if($subject_string){
+            // echo "<option>select subject</option>";
+            while($result = $subject_string->fetch_assoc()){
+                // $result['subject_id'];
+                $subject_array = explode(',', $result['subject_id']);
+
+                foreach($subject_array as $subject){
+                    $query_subject = "SELECT * FROM tbl_subject WHERE id = '$subject' ";
+                    $subject_list = $db->select($query_subject);
+
+                    if($subject_list){
+                        while($result = $subject_list->fetch_assoc()){
+                            // echo "<option>".$result['subject_name']."</option>";
+                            echo "<div class='single-subject'><input type='checkbox'><span>".$result['subject_name']."</span></div>";
+                        }
+                    }
+                }
+                // echo "<option>".$result['subject_id']."</option>";
+            }
+        }
+
+        // if($district_list){
+        //     echo "<option>Select district</option>";
+        //     while($result = $district_list->fetch_assoc()){
+        //         echo "<option value=".$result['dis_name'].">".ucfirst($result['dis_name'])."</option>";
+        //     }
+        // }else{
+        //     echo "<option>No district available</option>";
+        // }
+    }
+?>
